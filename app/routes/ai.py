ecd4468b509a75891e80ddcec1c1be38ac13fc11
Justin Johnson <jujohnso@akamai.com>
2025-07-08 02:11:40 +0530

    api endpoint name change

--- app/routes/ai.py
+++ app/routes/ai.py
@@ -3,5 +3,5 @@ from controllers.ai_controller import proofread, modify
 
 ai_bp = Blueprint('ai', __name__, url_prefix='/api')
 
-ai_bp.route('/proof-read', methods=['POST'])(proofread)
+ai_bp.route('/proofread', methods=['POST'])(proofread)
 ai_bp.route('/modify', methods=['POST'])(modify)
